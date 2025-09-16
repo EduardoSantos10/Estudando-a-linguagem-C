63b393cc0db0fa1142b1406b8266408f758cf9c6
Eduardo <eduardo25082002@gmail.com>
2025-09-16 19:38:32 -0300

    segundo commit

--- aula2.cpp
+++ aula2.cpp
@@ -2,6 +2,7 @@
 
 int main(){ // local onde meu codigo será executado
 
+    // Declaração de variavel
     int n, // int "inteiro"
     // "float" -> numeros quebrados (EX: 3.14, 0.5)
 
@@ -10,13 +11,14 @@ int main(){ // local onde meu codigo será executado
     printf("Digite um numero N: "); // scanner (solicitando um numero)
     scanf("%d", &n);
 
-    // o for percorre 10 vezes a nossa lista
-    for(int i= 1; <= n; i++){
+    // o for percorre de 1 a 10 vezes a nossa lista
+    for(int i= 1; i<= n; i++){ // o for é usado quantas vez que eu quero percorrer a minha lista
         soma += i; // soma = soma + 1
     }
 
+    printf("A soma dos numeros de 1 %d, é %d", n, soma );
+    return 0;
 
-
-
+    // "FOR" usamos quando a condição for verdadeira
 
 }
