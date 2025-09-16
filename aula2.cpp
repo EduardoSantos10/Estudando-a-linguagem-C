2451a3f03a5364e2da3483ba3293186e2c4d4891
Eduardo <eduardo25082002@gmail.com>
2025-09-16 19:16:24 -0300

    Primeiro Commit

---
+++ aula2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h> // biblioteca do C
+
+int main(){ // local onde meu codigo será executado
+
+    int n, // int "inteiro"
+    // "float" -> numeros quebrados (EX: 3.14, 0.5)
+
+    soma = 0; // atribuição de valor na variavel soma
+
+    printf("Digite um numero N: "); // scanner (solicitando um numero)
+    scanf("%d", &n);
+
+    // o for percorre 10 vezes a nossa lista
+    for(int i= 1; <= n; i++){
+        soma += i; // soma = soma + 1
+    }
+
+
+
+
+
+}
